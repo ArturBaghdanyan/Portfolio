188d323962938c690cdf74699679ed65f4277e38
ArturBaghdanyan <arturbaghdanyan20@gmail.com>
2025-05-23 21:32:05 +0400

    changed styles

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 
-import { AiFillCodepenCircle } from "react-icons/ai";
-import { IoMenuSharp } from "react-icons/io5";
+import "./style.css";
 
-import "./style.scss";
+import { AiFillCodepenCircle } from "react-icons/ai";
+
+import { IoMenuSharp } from "react-icons/io5";
 
 const Header = (props: any) => {
   const { myRef, aboutRef, projectsRef, contactRef } = props;
@@ -16,26 +17,26 @@ const Header = (props: any) => {
   };
 
   return (
-    <header className='flex flex-col text-white
+    <header className='header flex flex-col text-white relative
                        w-full justify-start mx-auto container-spacing'
     >
-      <div className='header_container flex items-center justify-between my-5'>
-        <div className='header_container_logo flex gap-x-3 items-center
-                        md:items-center md:flex-row md:gap-x-3'
+      <div className={`header_container flex items-center justify-between my-5`}>
+        <div className={`header_container_logo flex gap-x-3 items-center
+          md:items-center md:flex-row md:gap-x-3`}
         >
-          <AiFillCodepenCircle width={40} height={40} />
+          <AiFillCodepenCircle />
           <div className='flex-col'>
             <h3>Artur Baghdanyan</h3>
             <p>Front End developer</p>
           </div>
         </div>
 
-        <ul className='header_container_nav hidden uppercase cursor-pointer
-                       gap-3 no-underline md:flex lg:flex xl:flex'>
-          <li onClick={() => executeScroll(myRef)}>Home</li>
-          <li onClick={() => executeScroll(aboutRef)}>About</li>
-          <li onClick={() => executeScroll(projectsRef)}>Projects</li>
-          <li onClick={() => executeScroll(contactRef)}>Contact</li>
+        <ul className={`header_container_nav hidden uppercase cursor-pointer
+          gap-3 no-underline md:flex lg:flex xl:flex`}>
+          <li onClick={() => executeScroll(myRef)} className='relative grow'>Home</li>
+          <li onClick={() => executeScroll(aboutRef)} className='relative grow'>About</li>
+          <li onClick={() => executeScroll(projectsRef)} className='relative grow'>Projects</li>
+          <li onClick={() => executeScroll(contactRef)} className='relative grow'>Contact</li>
         </ul>
 
         <button
@@ -52,7 +53,7 @@ const Header = (props: any) => {
           className={`w-64 min-h-screen flex flex-col items-center fixed top-0 right-0 
                       placeholder-teal-400 pt-14 px-[40px] transition-transform ease-in-out 
                       duration-300 gap-y-5 bg-[#2F4F4F] text-[rgb(234,179,8)] overflow-x-hidden
-           ${showNav ? 'transform translate-x-[-250px] ease-in-out duration-500' : ""}`}
+           ${showNav ? 'transform translate-x-[-250px] ease-in-out duration-500' : ''}`}
         >
           <button className='w-8 h-8 text-white fixed z-10 top-4 right-1 text-center cursor-pointer'
                   onClick={() => setShowNav(!showNav)}>
